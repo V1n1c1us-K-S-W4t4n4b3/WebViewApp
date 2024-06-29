fd61b31fc237176e87703a4a6c31f2d18a6ca799
vinicius watanabe <vinikswatanabe@outlook.com>
2024-06-29 19:24:57 -0300

    first commit

--- WebViewApp/ContentView.swift
+++ WebViewApp/ContentView.swift
@@ -6,14 +6,75 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let url: String
+    @Binding var goback: Bool
+    @Binding var goForward: Bool
+    
+    func makeUIView(context: Context) -> WKWebView{
+        let wb = WKWebView()
+        
+        if let myURL = URL(string: url), UIApplication.shared.canOpenURL(myURL) {
+            let request = URLRequest(url: myURL)
+            wb.load(request)
+        }else{
+            let message = "ERROR - URL malformed :\(url)"
+            wb.loadHTMLString("<h1 style=\"font-size: 100px;\">\(message)</h1>", baseURL: nil)
+        }
+        
+        return wb
+    }
+    
+    func updateUIView(_ wb: WKWebView, context: Context) {
+        if goback {
+            wb.goBack()
+            goback = false
+        }else if goForward {
+            wb.goForward()
+            goForward = false
+        }
+    }
+}
 
 struct ContentView: View {
+    
+    @State private var isPresent = false
+    @State private var isGoBack = false
+    @State private var isGoForward = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Abrir Site"){
+                isPresent = true
+            }
+            .sheet(isPresented: $isPresent){
+                VStack{
+                    WebView(url: "https://github.com/V1n1c1us-K-S-W4t4n4b3",
+                            goback: $isGoBack, goForward: $isGoForward)
+                    
+                    HStack{
+                        Spacer()
+                        
+                        Button("Voltar"){
+                            isGoBack = true
+                            
+                        }
+                        
+                        Spacer()
+                        
+                        Button("Avançar"){
+                            isGoForward = true
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                .navigationTitle("Site")
+                .navigationBarTitleDisplayMode(.inline)
+            }
         }
         .padding()
     }
