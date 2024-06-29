01aefe43d3ac01b2dc450593e1240be7e5d77741
vinicius watanabe <vinikswatanabe@outlook.com>
2024-06-29 18:33:30 -0300

    Initial Commit

---
+++ WebViewApp/WebViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewAppApp.swift
+//  WebViewApp
+//
+//  Created by Usuario on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WebViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
